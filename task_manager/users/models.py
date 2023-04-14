b7f529fcf27cede11caececd55488b445e92f4c2
zluuba <zluyba.nikitina@gmail.com>
2023-04-14 15:34:33 +0300

    feat: add User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
