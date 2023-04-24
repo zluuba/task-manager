9dab8df85b3ba566d79c359729585969c4ef4087
zluuba <zluyba.nikitina@gmail.com>
2023-04-24 17:27:18 +0300

    fix: add fields to User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,6 +1,6 @@
 # from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User as Us
+# from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import User as Us
 from django.db import models
 # from django import forms
 
@@ -10,6 +10,8 @@ from django.db import models
 #     last_name = models.CharField(max_length=150)
 
 class User(models.Model):
-    first_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=150)   # verbose_name
     last_name = models.CharField(max_length=150)
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
