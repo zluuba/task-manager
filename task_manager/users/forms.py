cb00d3d94d447077789535f01cf75dc75d1cbb4d
zluuba <zluyba.nikitina@gmail.com>
2023-04-25 18:47:30 +0300

    fix: update forms

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,59 +1,56 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 FIELD_STYLE = 'form-control bg-dark text-white'
 
 
-class AddUserForm(forms.Form):
-    first_name = forms.CharField(max_length=255, label='First name',
-                                 widget=forms.TextInput(attrs={
-                                     'class': FIELD_STYLE,
-                                     'placeholder': 'First name'
-                                 }))
-    last_name = forms.CharField(max_length=255, label='Last name',
-                                widget=forms.TextInput(attrs={
-                                    'class': FIELD_STYLE,
-                                    'placeholder': 'Last name',
-                                }))
-    username = forms.CharField(max_length=255, label='Username',
-                               widget=forms.TextInput(attrs={
-                                   'class': FIELD_STYLE,
-                                   'placeholder': 'Username',
-                               }))
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput(attrs={
-                                   'class': FIELD_STYLE,
-                                   'placeholder': 'Password',
-                               }))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-                                    'class': FIELD_STYLE,
-                                    'placeholder': 'Confirm password',
-                               }))
+class RegisterUserForm(UserCreationForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'label': 'Password',
+            'class': FIELD_STYLE,
+            'placeholder': 'Password',
+        }))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'label': 'Confirm password',
+            'class': FIELD_STYLE,
+            'placeholder': 'Confirm password',
+        }))
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password')
+        fields = ['first_name', 'last_name', 'username']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': FIELD_STYLE, 'placeholder': 'First name'}),
+            'last_name': forms.TextInput(attrs={'class': FIELD_STYLE, 'placeholder': 'Last name'}),
+            'username': forms.TextInput(attrs={'class': FIELD_STYLE, 'placeholder': 'Username'}),
+        }
 
-    def clean(self):
-        cleaned_data = super(AddUserForm, self).clean()
-        password = cleaned_data.get("password")
-        confirm_password = cleaned_data.get("confirm_password")
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get("confirm_password")
+
+        if len(password) <= 3:
+            raise forms.ValidationError(
+                "Password too short"
+            )
 
         if password != confirm_password:
             raise forms.ValidationError(
                 "Passwords doesn't match"
             )
 
+        return password
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255, label='Username',
-                               widget=forms.TextInput(attrs={
-                                   'class': FIELD_STYLE,
-                                   'placeholder': 'Username',
-                               }))
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput(attrs={
-                                   'class': FIELD_STYLE,
-                                   'placeholder': 'Password',
-                               }))
+
+class LoginUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': FIELD_STYLE, 'placeholder': 'Username'}),
+            'password': forms.PasswordInput(attrs={'class': FIELD_STYLE, 'placeholder': 'Password'}),
+        }
