5a50dc3e194272fd2785914d7c1a9e94f15539bb
zluuba <zluyba.nikitina@gmail.com>
2023-04-25 18:47:54 +0300

    fix: update User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,17 +1,19 @@
 # from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 # from django.contrib.auth.models import User as Us
 from django.db import models
 # from django import forms
 
 
-# class User(AbstractUser):
-#     first_name = models.CharField(max_length=150)
-#     last_name = models.CharField(max_length=150)
-
-class User(models.Model):
-    first_name = models.CharField(max_length=150)   # verbose_name
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    username = models.CharField(max_length=150)
+    username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def get_fullname(self):
+        return f"{self.first_name} {self.last_name}"
