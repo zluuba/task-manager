6f0f7d275b01698b26e53e3b55f0a6961dbfc0f1
zluuba <zluyba.nikitina@gmail.com>
2023-04-26 17:53:21 +0300

    fix: update HomeView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,68 +1,28 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from task_manager.users.forms import LoginForm
+from django.views.generic.base import TemplateView
+from django.utils.translation import gettext as _
 
 
-def index(request):
-    title = 'Task manager'
-    content = {
-        'header': 'Hello there',
-        'greetings': 'This is the Task Manager,',
-        'body': 'I allow you to set tasks, assign performers and change their statuses. '
-                'Registration and authentication are required to work with my system.',
+TITLE = _('Task manager')
+GREET_ANON = _('Hello there')
+GREET_USER = _('Hello, ')
+TEXT = [_('This is the Task Manager,'),
+        _('I allow you to set tasks, assign performers and '
+          'change their statuses.'),
+        _('Registration and authentication are required to '
+          'work with my system.'),
+        ]
+
+
+class HomeView(TemplateView):
+    """
+    Figure out why "{% trans '' %}" doesn't work and move text to home template
+    """
+    template_name = 'index.html'
+    extra_context = {
+        'title': TITLE, 'greet_anon': GREET_ANON,
+        'greet_user': GREET_USER, 'text': TEXT,
     }
-    return render(request, 'index.html', context={
-        'title': title,
-        'content': content,
-    })
 
 
-class LoginView(View):
-
-    def get(self, request, *args, **kwargs):
-        title = 'Sign In'
-        form = LoginForm()
-        return render(request, 'login.html', context={
-            'title': title,
-            'form': form,
-        })
-
-    def post(self, request, *args, **kwargs):
-        title = 'Sign In'
-        form = LoginForm()
-        return render(request, 'login.html', context={
-            'title': title,
-            'form': form,
-        })
-
-
-class LogoutView(View):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
-
-
-def page_not_found(request, exception):
-    title = 'Page not found'
-    content = {
-        'sad_smile': '˙◠˙',
-        'message': 'Page not found',
-        'url_text': 'Go home'
-    }
-    return render(request, "page_not_found.html", context={
-        'title': title,
-        'content': content,
-    })
-
-
-def internal_server_error(request):
-    title = 'Internal server error'
-    content = {
-        'sad_smile': '˙◠˙',
-        'message': 'Internal server error',
-        'url_text': 'Go home'
-    }
-    return render(request, "internal_server_error.html", context={
-        'title': title,
-        'content': content,
-    })
+def test_case(request):
+    raise Exception('This is a test error')
