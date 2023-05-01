17c84c7a3310d73a35359ce2aff87f05c61064c6
zluuba <zluyba.nikitina@gmail.com>
2023-05-01 13:58:42 +0300

    fix: update task model, migrations

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -20,7 +20,7 @@ class Task(models.Model):
     )
     executor = models.ForeignKey(
         User, related_name='executor', verbose_name=_('Executor'),
-        null=True, blank=True, default=None, on_delete=models.PROTECT,
+        null=True, blank=True, default='', on_delete=models.PROTECT,
     )
     status = models.ForeignKey(
         Status, related_name='status', verbose_name=_('Status'),
