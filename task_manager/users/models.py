053f66bf4e3bc51e4bee361f11d2a52ace21999c
zluuba <zluyba.nikitina@gmail.com>
2023-05-01 14:57:49 +0300

    fix: update user model, migrations

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,5 +6,8 @@ from django.db import models
 class User(Us, AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.get_fullname()
+
     def get_fullname(self):
         return f"{self.first_name} {self.last_name}"
